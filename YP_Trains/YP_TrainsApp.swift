4331ccb9f65f2f1909a1b88bc191e05f1e55b988
zeons <oao@i-cio.ru>
2026-04-25 08:47:58 +0300

    Initial Commit

---
+++ YP_Trains/YP_TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YP_TrainsApp.swift
+//  YP_Trains
+//
+//  Created by Oschepkov Aleksandr on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct YP_TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
